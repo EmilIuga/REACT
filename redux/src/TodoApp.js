3a3889fba6e40bd426a0b9e0edf315e080361c4d
WHQ_NT_DOMAIN\EI083499 <emilbdb@gmail.com>
2020-08-20 12:58:10 +0300

    Tema3

---
+++ redux/src/TodoApp.js
@@ -0,0 +1,19 @@
+import * as React from 'react'
+import ProgressBarSize from './components/progressbar/progressbar'
+import PaddingTable from './components/table/table'
+
+// export const TodoApp = () => {
+//   return (
+//     <div>
+//       <h1>This is a table</h1>
+//       <PaddingTable></PaddingTable>
+//       <h1>This is a progress bar</h1>
+//       <ProgressBarSize></ProgressBarSize>
+//     </div>
+//   )
+// }
+
+function TodoApp() {
+  return <PaddingTable></PaddingTable>
+}
+export default TodoApp
